eff492a6e122261e14a8bf139a1aa5a2d380b758
Kent Fredric <kentfredric@gmail.com>
2016-04-29 19:40:25 +1200

    v0.001001
     - Revised and improved recommendations around namespacing and packaging

--- lib/Dist/Zilla/Plugin/Beam/Connector.pm
+++ lib/Dist/Zilla/Plugin/Beam/Connector.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Beam::Connector;
 
-our $VERSION = '0.001001';
+our $VERSION = '0.001002';
 
 # ABSTRACT: Connect events to listeners in Dist::Zilla plugins.
 
