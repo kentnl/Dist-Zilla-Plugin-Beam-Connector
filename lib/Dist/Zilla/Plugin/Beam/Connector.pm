71b2c5eecc73b15893a18eaa93cb4d0644b03ae2
Kent Fredric <kentfredric@gmail.com>
2016-04-29 18:43:56 +1200

    v0.001000
     - First version.

--- lib/Dist/Zilla/Plugin/Beam/Connector.pm
+++ lib/Dist/Zilla/Plugin/Beam/Connector.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Beam::Connector;
 
-our $VERSION = '0.001000';
+our $VERSION = '0.001001';
 
 # ABSTRACT: Connect events to listeners in Dist::Zilla plugins.
 
